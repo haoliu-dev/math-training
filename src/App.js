94c9d2e0f8e2766effdaa6ee8617009ed38f8d91
Hao Liu <haoliu.dev@gmail.com>
2020-05-05 17:50:28 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>4 + 55 = 
+      <input type="text" value="33"/>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
