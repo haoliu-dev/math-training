f6affab336143f3b5017efe36dd215f75a5f1393
Hao Liu <haoliu.dev@gmail.com>
2020-05-05 18:28:20 -0700

    create sheet

--- src/App.js
+++ src/App.js
@@ -3,12 +3,34 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>4 + 55 = 
-      <input type="text" value="33"/>
-      </h1>
+    <div className="App">      
+    <div className="container-fluid">
+      <Sheet count="66" />  
+    </div>
+      
     </div>
   );
 }
 
 export default App;
+
+const OPERATION = ['+', '-'];
+class Question extends React.Component {
+  render() {
+    const op1 = Math.floor(Math.random() * 100);
+    const op2 = Math.floor(Math.random() * 100);
+    const operation = OPERATION[Math.floor(Math.random() * 100) % OPERATION.length];
+    return (
+      <div className="col-4 question">{op1} {operation} {op2} = </div>
+
+    );
+  }
+}
+
+class Sheet extends React.Component {
+  render() {
+    const count = parseInt(this.props.count) || 100;
+    const questions = (Array(count).fill(1)).map(x => (<Question />));
+    return <div className="row">{questions}</div>
+  }
+}
